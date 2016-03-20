d3c50f26ec66bd9a461a5239ac685efe4b71684b
Varfalomey <qaoble@inbox.ru>
2016-03-20 16:48:09 +0400

    Update Main.php
    Removed player drop

--- src/ImagicalGamer/NoDrop/Main.php
+++ src/ImagicalGamer/NoDrop/Main.php
@@ -1,18 +1,24 @@
 <?php
+
 namespace ImagicalGamer\NoDrop;
-use pocketmine\command\CommandSender;
-use pocketmine\command\Command;
+
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDeathEvent;
 use pocketmine\event\player\PlayerDropItemEvent;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat as Color;
+
 class Main extends PluginBase implements Listener{
 
-public function onEnable(){
-	$this->getServer()->getPluginManager()->registerEvents($this, $this);
-	$this->getLogger()->info(Color::GREEN . "No-Drop Enabled!");
-}
-public function onDropItem(PlayerDropItemEvent $event){
-    $event->setCancelled(true);
-}
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info(Color::GREEN . "No-Drop Enabled!");
+    }
+    public function PlayerDeathEvent(PlayerDeathEvent $event){
+        $event->setDrops(array());
+    }
+    public function onDropItem(PlayerDropItemEvent $event){
+        $event->setCancelled(true);
+    }
+    
 }
